f294f63e65c4e3af8f0b9458f04882bb2221cba4
DuyTranDuc <duytranduc0204@gmail.com>
2023-04-17 14:33:58 +0700

    Update lan 2

---
+++ main.js
@@ -0,0 +1,33 @@
+function signup(e){
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var address = document.getElementById("address").value;
+    var password = document.getElementById("password").value;
+    var user = {
+        // username: username,
+        email: email,
+        // address: address,
+        password: password,
+    };
+    var json = JSON.stringify(user);
+    localStorage.setItem(email, json);
+    alert("Đăng ký thành công !");
+}
+function login(e){
+    event.preventDefault();
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var user = localStorage.getItem(email);
+    var data = JSON.parse(user);
+    if(user == null){
+        alert("Vui lòng nhập đầy đủ thông tin !");
+    }
+    else if(email == data.email && password == data.password){
+        alert("Đăng nhập thành công !");
+        window.location.href = "index.html"
+    }
+    else{
+        alert("Đăng nhập thất bại !");
+    }
+}
